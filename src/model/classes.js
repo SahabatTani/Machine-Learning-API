0ecb99f08ee2e3f814160643040640e980575001
jihadable <umarjihad141004@gmail.com>
2025-06-04 10:46:54 +0700

    add rice machine learning model

--- src/model/classes.js
+++ src/model/classes.js
@@ -1,6 +1,6 @@
 const classes = {
     jagung: ["Blight", "Common Rust", "Grey Leaf Spot", "Healthy"],
-    padi: [],
+    padi: ["Bacterial Leaf Blight", "Brown Spot", "Healthy", "Hispa", "Leaf Blast", "Leaf Scald", "Narrow Brown Leaf Spot", "Neck Blast", "Sheath Blight", "Tungro"],
     singkong: [],
     kentang: ["Bacteria", "Fungi", "Healthy", "Nematode", "Pest", "Phytopthora", "Virus"]
 }
--- src/model/model.js
+++ src/model/model.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const tf = require('@tensorflow/tfjs-node');
-
+const fs = require("fs").promises;
 
 const loadModel = async (modelName) => {
-    const modelPath = `file://${path.resolve(__dirname, `${modelName}/model.json`)}`
-    const model = await tf.loadGraphModel(modelPath)
+    const modelDir = path.resolve(__dirname, modelName);
+    const modelJsonPath = path.join(modelDir, "model.json");
+    const modelJson = await fs.readFile(modelJsonPath, "utf8");
+    const parsed = JSON.parse(modelJson);
 
-    return model
+    const modelPath = `file://${modelJsonPath}`;
+
+    if (parsed.format === "graph-model") {
+        return await tf.loadGraphModel(modelPath);
+    } else if (parsed.format === "layers-model") {
+        return await tf.loadLayersModel(modelPath);
+    } else {
+        throw new Error("Unknown model format");
+    }
 }
 
 module.exports = { loadModel }

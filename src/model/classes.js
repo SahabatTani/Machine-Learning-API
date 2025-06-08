6583871a68cf40539da9be37fc85f11d73196a51
jihadable <umarjihad141004@gmail.com>
2025-06-08 18:49:25 +0700

    add singkong model

--- src/model/classes.js
+++ src/model/classes.js
@@ -1,7 +1,7 @@
 const classes = {
     jagung: ["Blight", "Common Rust", "Gray Leaf Spot", "Sehat"],
     padi: ["Bacterial Leaf Blight", "Brown Spot", "Sehat", "Hispa", "Leaf Blast", "Leaf Scald", "Narrow Brown Leaf Spot", "Neck Blast", "Sheath Blight", "Tungro"],
-    singkong: [],
+    singkong: ["Bacterial Blight", "Brown Streak Disease", "Green Mottle", "Sehat", "Mosaic Disease"],
     mangga: ["Antharacnose", "Bacterial Canker", "Cutting Weevil", "Die Back", "Gall Midge", "Sehat", "Powdery Mildew", "Sooty Mould",]
 }
 
